5f6e939e02f7b7ad0d1516d573d627d541f5059e
Chinito4852 <tristinowong@gmail.com>
2021-03-18 16:57:41 -0700

    first commit to heroku

---
+++ README.md
@@ -0,0 +1,19 @@
+# Fun quotes API
+A simple Node.js server because I wanted to put some of my favorite quotes from APIs I've played around with in a single place.
+Credits: Andrew Jazbec for Kanye.rest, gillescoolen for taylor.rest, jamesseanwright for the Ron Swanson quotes API, and Alty,Inc. for Forismatic.
+
+## Routes
+
+### `GET /kanye`
+
+### `GET /taylor`
+
+### `GET /ron`
+
+### `GET /inspirational`
+
+### `GET /random`
+
+### Local development
+After cloning this repo, run 'npm install' or 'npm i' to install the required dependencies.
+Then, run 'npm start' to run the server.
---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Parse the request body into an JavaScript object and assign it to req.body
+app.use(bodyParser.json());
+
+//https://api.kanye.rest/
+//https://api.taylor.rest/
+//http://ron-swanson-quotes.herokuapp.com/v2/quotes
+//http://api.forismatic.com/api/1.0/?lang=en&method=getQuote&format=json
+
+const Quote = function(quote, author) {
+  this.quote = quote;
+  this.author = author;
+}
+
+app.get('/kanye', function(req, res) {
+  request("https://api.kanye.rest/", {json: true}, function(err, result, body) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      res.send(new Quote(body.quote, "Kanye West"));
+    }
+  });
+});
+
+app.get('/taylor', function(req, res) {
+  request("https://api.taylor.rest/", {json: true}, function(err, result, body) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/ron', function(req, res) {
+  request("http://ron-swanson-quotes.herokuapp.com/v2/quotes", {json: true}, function(err, result, body) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      console.log(body[0]);
+      res.send(body);
+    }
+  });
+});
+
+app.get('/inspirational', function(req, res) {
+  request("http://api.forismatic.com/api/1.0/?lang=en&method=getQuote&format=json", {json: true}, function(err, result, body) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      res.send(body);
+    }
+  });
+});
+
+app.get('/random', function(req, res) {
+  let url = "";
+  const source = Math.floor(Math.random() * Math.floor(4));
+  if (source === 0) url = "https://api.kanye.rest/";
+  else if (source === 1) url = "https://api.taylor.rest/";
+  else if (source === 2) url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+  else url = "http://api.forismatic.com/api/1.0/?lang=en&method=getQuote&format=json";
+
+  request(url, {json: true}, function(err, result, body) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      if (source === 0) res.send(new Quote(body.quote, "Kanye West"));
+      else if (source === 1) res.send(new Quote(body.quote, "Taylor Swift"));
+      else if (source === 2) res.send(new Quote(body[0], "Ron Swanson"));
+      else res.send(new Quote(body.quoteText, body.quoteAuthor));
+    }
+  });
+});
+
+
+// Listen on port
+app.listen(port, function() {
+  console.log("Server started.");
+});
